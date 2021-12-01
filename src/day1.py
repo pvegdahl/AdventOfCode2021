e6c92e90c35af878628b5c68dc00bb7a8c40921e
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-01 15:24:55 -0500

    Black autoformatting

--- src/day1.py
+++ src/day1.py
@@ -3,15 +3,18 @@ from typing import List
 import pytest
 
 
-@pytest.mark.parametrize("input_list, expected_count", [
-    ([], 0),
-    ([1], 0),
-    ([1, 0], 0),
-    ([1, 2], 1),
-    ([1, 2, 1], 1),
-    ([1, 2, 3], 2),
-    ([1, 2, 3, 2, 4, 10, 4, 2, 6], 5),
-])
+@pytest.mark.parametrize(
+    "input_list, expected_count",
+    [
+        ([], 0),
+        ([1], 0),
+        ([1, 0], 0),
+        ([1, 2], 1),
+        ([1, 2, 1], 1),
+        ([1, 2, 3], 2),
+        ([1, 2, 3, 2, 4, 10, 4, 2, 6], 5),
+    ],
+)
 def test_count_increase(input_list, expected_count):
     assert count_increase(input_list) == expected_count
 
@@ -21,20 +24,23 @@ def count_increase(numbers: List[int]) -> int:
     if len(numbers) < 2:
         return count
 
-    for i in range(len(numbers)-1):
-        if numbers[i] < numbers[i+1]:
+    for i in range(len(numbers) - 1):
+        if numbers[i] < numbers[i + 1]:
             count += 1
     return count
 
 
-@pytest.mark.parametrize("input_string, expected_list", [
-    ("", []),
-    ("123", [123]),
-    ("1\n2", [1, 2]),
-    ("\n\n", []),
-    ("1\n2\n", [1, 2]),
-    ("1  \n   2\n3\n4", [1, 2, 3, 4]),
-])
+@pytest.mark.parametrize(
+    "input_string, expected_list",
+    [
+        ("", []),
+        ("123", [123]),
+        ("1\n2", [1, 2]),
+        ("\n\n", []),
+        ("1\n2\n", [1, 2]),
+        ("1  \n   2\n3\n4", [1, 2, 3, 4]),
+    ],
+)
 def test_get_numbers_from_string(input_string, expected_list):
     assert get_numbers_from_string(input_string) == expected_list
 
@@ -50,21 +56,24 @@ def get_numbers_from_string(input_string: str) -> List[int]:
     return result
 
 
-@pytest.mark.parametrize("input_list, expected_list", [
-    ([], []),
-    ([1], []),
-    ([1, 2], []),
-    ([1, 2, 3], [6]),
-    ([1, 2, 3, 4], [6, 9]),
-    ([1, 2, 3, 4, 5], [6, 9, 12]),
-    ([1, 2, 1, 4, 10, -4], [4, 7, 15, 10]),
-])
+@pytest.mark.parametrize(
+    "input_list, expected_list",
+    [
+        ([], []),
+        ([1], []),
+        ([1, 2], []),
+        ([1, 2, 3], [6]),
+        ([1, 2, 3, 4], [6, 9]),
+        ([1, 2, 3, 4, 5], [6, 9, 12]),
+        ([1, 2, 1, 4, 10, -4], [4, 7, 15, 10]),
+    ],
+)
 def test_calculate_sliding_window_sums(input_list, expected_list):
     assert calculate_sliding_window_sums(input_list) == expected_list
 
 
 def calculate_sliding_window_sums(input_list: List[int]) -> List[int]:
-    return [sum(input_list[i:i + 3]) for i in range(len(input_list) - 2)]
+    return [sum(input_list[i : i + 3]) for i in range(len(input_list) - 2)]
 
 
 def day_1a(filepath: str) -> int:
