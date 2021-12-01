60cef67f446727508f9a428d6253c18ecfe5501e
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-01 14:42:57 -0500

    Working algorithm

--- src/day1.py
+++ src/day1.py
@@ -8,12 +8,20 @@ import pytest
     ([1], 0),
     ([1, 0], 0),
     ([1, 2], 1),
+    ([1, 2, 1], 1),
+    ([1, 2, 3], 2),
+    ([1, 2, 3, 2, 4, 10, 4, 2, 6], 5),
 ])
 def test_count_increase(input_list, expected_count):
     assert count_increase(input_list) == expected_count
 
 
 def count_increase(numbers: List[int]) -> int:
+    count = 0
     if len(numbers) < 2:
-        return 0
-    return 1 if numbers[0] < numbers[1] else 0
+        return count
+
+    for i in range(len(numbers)-1):
+        if numbers[i] < numbers[i+1]:
+            count += 1
+    return count
