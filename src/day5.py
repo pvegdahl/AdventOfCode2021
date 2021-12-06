2abd69f5e08e278ff4bfaa411901fa55e0aa7c35
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-06 10:44:27 -0500

    5A solved

--- src/day5.py
+++ src/day5.py
@@ -1,4 +1,5 @@
-from typing import List, NamedTuple
+from collections import defaultdict
+from typing import List, NamedTuple, Set, Dict
 
 import pytest
 
@@ -28,11 +29,16 @@ class LineSegment(NamedTuple):
         end = Point.from_string(points[1])
         return cls(start, end)
 
-    def get_points(self):
-        result = {self.start, self.end}
-        [first, last] = sorted([self.start.y, self.end.y])
-        for y in range(first, last):
-            result.add(Point(self.start.x, y))
+    def get_points(self) -> Set[Point]:
+        result = set()
+        if self.start.x == self.end.x:
+            [first, last] = sorted([self.start.y, self.end.y])
+            for y in range(first, last + 1):
+                result.add(Point(self.start.x, y))
+        elif self.start.y == self.end.y:
+            [first, last] = sorted([self.start.x, self.end.x])
+            for x in range(first, last + 1):
+                result.add(Point(x, self.start.y))
         return result
 
 
@@ -76,15 +82,127 @@ def parse_input(input_string) -> List[LineSegment]:
             LineSegment(start=Point(0, 3), end=Point(0, 0)),
             {Point(0, 0), Point(0, 1), Point(0, 2), Point(0, 3)},
         ),
+        (
+            LineSegment(start=Point(5, 6), end=Point(7, 6)),
+            {Point(5, 6), Point(6, 6), Point(7, 6)},
+        ),
+        (
+            LineSegment(start=Point(345, 678), end=Point(342, 678)),
+            {
+                Point(342, 678),
+                Point(343, 678),
+                Point(344, 678),
+                Point(345, 678),
+            },
+        ),
+        (LineSegment(start=Point(1, 2), end=Point(3, 4)), set()),
     ],
 )
 def test_get_points_on_line_segment(line_segment, expected):
     assert line_segment.get_points() == expected
 
 
+@pytest.mark.parametrize(
+    "line_segments, expected",
+    [
+        (
+            [LineSegment(start=Point(0, 0), end=Point(0, 3))],
+            {Point(0, 0): 1, Point(0, 1): 1, Point(0, 2): 1, Point(0, 3): 1},
+        ),
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+            ],
+            {Point(0, 0): 2, Point(0, 1): 2, Point(0, 2): 2, Point(0, 3): 2},
+        ),
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(0, 0), end=Point(0, 1)),
+            ],
+            {Point(0, 0): 2, Point(0, 1): 2, Point(0, 2): 1, Point(0, 3): 1},
+        ),
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(2, 2), end=Point(-1, 2)),
+            ],
+            {
+                Point(0, 0): 1,
+                Point(0, 1): 1,
+                Point(0, 2): 2,
+                Point(0, 3): 1,
+                Point(2, 2): 1,
+                Point(1, 2): 1,
+                Point(-1, 2): 1,
+            },
+        ),
+    ],
+)
+def test_sum_points(line_segments, expected):
+    assert sum_points(line_segments) == expected
+
+
+def sum_points(line_segments) -> Dict[Point, int]:
+    result = defaultdict(lambda: 0)
+    for line_segment in line_segments:
+        for point in line_segment.get_points():
+            result[point] += 1
+    return result
+
+
+@pytest.mark.parametrize(
+    "line_segments, expected",
+    [
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(2, 2), end=Point(-1, 2)),
+            ],
+            {Point(0, 2)},
+        ),
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(0, 1), end=Point(0, 2)),
+                LineSegment(start=Point(2, 2), end=Point(-1, 2)),
+            ],
+            {Point(0, 2), Point(0, 1)},
+        ),
+        (
+            [
+                LineSegment(start=Point(0, 0), end=Point(0, 3)),
+                LineSegment(start=Point(0, 1), end=Point(0, 2)),
+                LineSegment(start=Point(2, 2), end=Point(-1, 2)),
+                LineSegment(start=Point(2, 2), end=Point(-1, 2)),
+                LineSegment(start=Point(100, 101), end=Point(200, 101)),
+                LineSegment(start=Point(-1, -1), end=Point(1, 1)),
+            ],
+            {Point(0, 2), Point(0, 1), Point(2, 2), Point(1, 2), Point(-1, 2)},
+        ),
+    ],
+)
+def test_filter_to_two_plus(line_segments, expected):
+    assert filter_to_two_plus(line_segments) == expected
+
+
+def filter_to_two_plus(line_segments) -> Set[Point]:
+    point_counts = sum_points(line_segments)
+    result = {point for point, count in point_counts.items() if count > 1}
+    return result
+
+
+# def filter_to_two_plus_2(line_segments) -> Set[Point]:
+#     seen_once = set()
+#     seen_multiple_times = set()
+#     for line_segment in line_segments:line_segments
+
+
 def day5a(filepath: str) -> int:
     with open(filepath, "r") as file:
-        pass
+        input_string = file.read()
+    return len(filter_to_two_plus(parse_input(input_string)))
 
 
 def day5b(filepath: str) -> int:
@@ -94,4 +212,4 @@ def day5b(filepath: str) -> int:
 
 if __name__ == "__main__":
     print(f"The answer to 5A is: {day5a('../puzzle_input/day5.txt')}")
-    print(f"The answer to 5B is: {day2a('../puzzle_input/day5.txt')}")
+    print(f"The answer to 5B is: {day5b('../puzzle_input/day5.txt')}")
