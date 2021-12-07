5f9806de4b93b9d2f6f3c058f7e423e7c60f5f95
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-07 09:24:12 -0500

    Solving best fuel and position

--- src/day7.py
+++ src/day7.py
@@ -1,4 +1,5 @@
-from typing import List
+from statistics import mean
+from typing import List, Tuple
 
 import pytest
 
@@ -19,6 +20,47 @@ def parse_input(input_string) -> List[int]:
     if not input_string:
         return []
     return [int(number) for number in input_string.strip().split(",")]
+
+
+@pytest.mark.parametrize(
+    "positions, expected",
+    [
+        ([1], (1, 0)),
+        ([1, 2, 3], (2, 2)),
+        ([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], (2, 37)),
+    ],
+)
+def test_best_position_and_fuel(positions, expected):
+    assert best_position_and_fuel(positions) == expected
+
+
+def best_position_and_fuel(positions) -> Tuple[int, int]:
+    min_pos = min(positions)
+    max_pos = max(positions)
+    best_position = None
+    best_fuel = 1e100
+    for pos in range(min_pos, max_pos + 1):
+        fuel = calculate_fuel(current_positions=positions, target_position=pos)
+        if fuel < best_fuel:
+            best_position = pos
+            best_fuel = fuel
+    return best_position, best_fuel
+
+
+@pytest.mark.parametrize(
+    "current_positions, target_position, expected",
+    [
+        ([1], 1, 0),
+        ([1, 2, 3], 2, 2),
+        ([16, 1, 2, 0, 4, 2, 7, 1, 2, 14], 2, 37),
+    ],
+)
+def test_calculate_fuel(current_positions, target_position, expected):
+    assert calculate_fuel(current_positions, target_position) == expected
+
+
+def calculate_fuel(current_positions: List[int], target_position: int) -> int:
+    return sum([abs(pos - target_position) for pos in current_positions])
 
 
 def day7a(filepath: str) -> int:
