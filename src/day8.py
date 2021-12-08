17cc0a27016a8bb739be7abdc423a565f414348f
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-08 14:46:17 -0500

    Count lengths

--- src/day8.py
+++ src/day8.py
@@ -1,6 +1,7 @@
 import functools
+from collections import defaultdict
 from statistics import mean
-from typing import List, Tuple, Callable
+from typing import List, Tuple, Callable, Dict
 
 import pytest
 
@@ -31,6 +32,23 @@ def test_parse_one_line_of_output_signal_patterns(input_line, expected):
 
 def parse_one_line_of_output_signal_patterns(input_line) -> List[str]:
     return input_line.split("|")[1].strip().split(" ")
+
+
+@pytest.mark.parametrize(
+    "input_patterns, expected",
+    [
+        (["a", "ab", "abc", "abcd", "abcde", "abcdef", "def", "dcba", "xyz"], {1: 1, 2: 1, 3: 3, 4: 2, 5: 1, 6: 1})
+    ],
+)
+def test_count_by_lengths(input_patterns, expected):
+    assert count_by_lengths(input_patterns) == expected
+
+
+def count_by_lengths(input_patterns: List[str]) -> Dict[int, int]:
+    result = defaultdict(lambda: 0)
+    for pattern in input_patterns:
+        result[len(pattern)] += 1
+    return result
 
 
 def day8a(filepath: str) -> int:
