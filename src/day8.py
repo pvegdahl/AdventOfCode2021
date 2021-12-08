c351cdd3e501e4b0c3cdd6274bd6eab631520e0d
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-08 14:35:30 -0500

    First parse implemented

---
+++ src/day8.py
@@ -0,0 +1,34 @@
+import functools
+from statistics import mean
+from typing import List, Tuple, Callable
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_line, expected",
+    [
+        ("abc def ghi | anything it does not matter", ["abc", "def", "ghi"])
+    ],
+)
+def test_parse_one_line_of_input_signal_patterns(input_line, expected):
+    assert parse_one_line_of_input_signal_patterns(input_line) == expected
+
+
+def parse_one_line_of_input_signal_patterns(input_line) -> List[str]:
+    return input_line.split("|")[0].strip().split(" ")
+
+
+def day8a(filepath: str) -> int:
+    with open(filepath, "r") as file:
+        pass
+
+
+def day8b(filepath: str) -> int:
+    with open(filepath, "r") as file:
+        pass
+
+
+if __name__ == "__main__":
+    print(f"The answer to 8A is: {day8a('../puzzle_input/day8.txt')}")
+    print(f"The answer to 8B is: {day8b('../puzzle_input/day8.txt')}")
