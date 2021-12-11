2515ea2298690b02f35142a14009aab1c87026b6
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-11 18:26:43 -0500

    Find members over threshold works

--- src/day11.py
+++ src/day11.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Any
+from typing import List, Tuple
 
 import pytest
 
@@ -23,12 +23,32 @@ def parse_input(input_string):
 
 def test_add_one_to_everything(test_matrix):
     assert add_one_to_everything(test_matrix) == [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    assert test_matrix != add_one_to_everything(test_matrix)  # Do not mutate the input
 
 
 def add_one_to_everything(matrix: List[List[int]]) -> List[List[int]]:
     result = []
     for row in matrix:
         result.append([x + 1 for x in row])
+    return result
+
+
+@pytest.mark.parametrize("threshold, expected", [
+    (300, []),
+    (9, []),
+    (8, [(2, 2)]),
+    (4, [(1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]),
+])
+def test_find_cells_greater_than_threshold(threshold, expected, test_matrix):
+    assert sorted(find_cells_greater_than_threshold(matrix=test_matrix, threshold=threshold)) == sorted(expected)
+
+
+def find_cells_greater_than_threshold(matrix: List[List[int]], threshold: int = 9) -> List[Tuple[int, int]]:
+    result = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] > threshold:
+                result.append((i, j))
     return result
 
 
