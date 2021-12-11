5e4d8e23525205349581bd0799f9ba2aa747db91
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-11 18:15:54 -0500

    Parse test passes

--- src/day11.py
+++ src/day11.py
@@ -3,9 +3,16 @@ from typing import Optional, List, Any
 import pytest
 
 
+@pytest.fixture()
+def test_matrix():
+    return [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
 @pytest.mark.parametrize(
     "input_string, expected",
     [
+        ("", []),
+        ("123\n456\n789", [[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
     ],
 )
 def test_parse_input(input_string, expected):
@@ -13,7 +20,20 @@ def test_parse_input(input_string, expected):
 
 
 def parse_input(input_string):
+    if not input_string:
+        return []
+    result = []
+    for line in input_string.strip().split("\n"):
+        result.append([int(char) for char in line.strip()])
+    return result
+
+
+
+@pytest.mark.parametrize("matrix, expected", [
+])
+def test_add_one_to_everything(matrix, expected):
     pass
+
 
 
 def day11a(filepath: str) -> int:
