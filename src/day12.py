26ae42c3d09d4b5c792a8bfbdd19efa3e9c16721
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-12 15:04:17 -0500

    More passing parse tests

--- src/day12.py
+++ src/day12.py
@@ -6,6 +6,8 @@ import pytest
 @pytest.mark.parametrize("input_string, expected", [
     ("", {}),
     ("A-B", {"A": {"B"}, "B": {"A"}}),
+    ("A-B\nc-d", {"A": {"B"}, "B": {"A"}, "c": {"d"}, "d": {"c"}}),
+    ("A-B\nc-B", {"A": {"B"}, "B": {"A", "c"}, "c": {"B"}}),
 ])
 def test_parse_input(input_string, expected):
     assert parse_input(input_string) == expected
@@ -16,12 +18,10 @@ def parse_input(input_string):
     if not input_string:
         return result
 
-    [location_a, location_b] = input_string.strip().split("-")
-    result[location_a].add(location_b)
-    result[location_b].add(location_a)
-
-    # for line in input_string.strip().split("\n"):
-    #     pass
+    for line in input_string.strip().split("\n"):
+        [location_a, location_b] = line.strip().split("-")
+        result[location_a].add(location_b)
+        result[location_b].add(location_a)
     return result
 
 
