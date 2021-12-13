577d99d06400fac9216f85ddc687a407d1f2f9f6
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-13 09:15:15 -0500

    Parsing points done

---
+++ src/day13.py
@@ -0,0 +1,68 @@
+from typing import NamedTuple, Set, List
+
+import pytest
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    @classmethod
+    def from_string(cls, input_string):
+        [x, y] = input_string.strip().split(",")
+        return Point(x=int(x), y=int(y))
+
+
+@pytest.mark.parametrize("input_string, expected", [
+    ("", set()),
+    ("1, 2", {Point(1, 2)}),
+    ("86,99", {Point(86, 99)}),
+    ("1,2\n3,4\n5,6\n", {Point(1, 2), Point(3, 4), Point(5, 6)}),
+    ("1,2\n3,4\n5,6\n\nfold along x=42\nfold along y=47", {Point(1, 2), Point(3, 4), Point(5, 6)}),
+])
+def test_parse_input_points(input_string, expected):
+    assert parse_input_points(input_string) == expected
+
+
+def parse_input_points(input_string) -> Set[Point]:
+    result = set()
+    for line in input_string.strip().split("\n"):
+        if line:
+            result.add(Point.from_string(line))
+        else:
+            break
+    return result
+
+
+class Fold(NamedTuple):
+    dimension: str
+    coordinate: int
+
+
+@pytest.mark.parametrize("input_string, expected", [
+    ("", []),
+    ])
+def test_parse_input_folds(input_string, expected):
+    assert parse_input_folds(input_string) == expected
+
+
+def parse_input_folds(input_string: str) -> List[Fold]:
+    return []
+
+
+
+def part_a(filepath: str):
+    with open(filepath, "r") as file:
+        pass
+
+
+def part_b(filepath: str):
+    with open(filepath, "r") as file:
+        pass
+
+
+if __name__ == "__main__":
+    day = 13
+    input_file = f"../puzzle_input/day{day}.txt"
+    print(f"The answer to {day}A is: {part_a(input_file)}")
+    print(f"The answer to {day}B is: {part_b(input_file)}")
