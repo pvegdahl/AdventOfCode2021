49e0526aad7a5f774e512df218c7f56f04284cb3
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-13 09:50:09 -0500

    Support folds in both directions and test on AOC example

--- src/day13.py
+++ src/day13.py
@@ -46,7 +46,7 @@ class Fold(NamedTuple):
 
     @classmethod
     def from_string(cls, input_string: str) -> "Fold":
-        [dimension, coordinate] = input_string[11:].strip().split("=")
+        [dimension, coordinate] = input_string.strip()[11:].split("=")
         return Fold(dimension=dimension, coordinate=int(coordinate))
 
 
@@ -89,18 +89,92 @@ def parse_input_folds(input_string: str) -> List[Fold]:
             Fold("x", 3),
             {Point(1, 0), Point(2, 0), Point(1, 1), Point(0, 2)},
         ),
+        (
+                {Point(0, 1), Point(0, 4), Point(1, 5), Point(2, 6)},
+                Fold("y", 3),
+                {Point(0, 1), Point(0, 2), Point(1, 1), Point(2, 0)},
+        ),
     ],
 )
 def test_fold(points, a_fold, expected):
     assert fold(points=points, a_fold=a_fold) == expected
 
 
+@pytest.fixture()
+def aoc_input_text() -> str:
+    return """6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+
+    fold along y=7
+    fold along x=5"""
+
+
+def test_fold_aoc_example(aoc_input_text):
+    points = parse_input_points(aoc_input_text)
+    folds = parse_input_folds(aoc_input_text)
+    for a_fold in folds:
+        points = fold(points=points, a_fold=a_fold)
+    assert points == {
+        Point(0, 0),
+        Point(1, 0),
+        Point(2, 0),
+        Point(3, 0),
+        Point(4, 0),
+        Point(0, 1),
+        Point(0, 2),
+        Point(0, 3),
+        Point(0, 4),
+        Point(4, 1),
+        Point(4, 2),
+        Point(4, 3),
+        Point(4, 4),
+        Point(1, 4),
+        Point(2, 4),
+        Point(3, 4),
+    }
+
+
 def fold(points: Set[Point], a_fold: Fold) -> Set[Point]:
+    if a_fold.dimension == "x":
+        return fold_on_x(a_fold, points)
+    elif a_fold.dimension == "y":
+        return fold_on_y(a_fold, points)
+    else:
+        raise Exception(f"Cannot fold on {a_fold.dimension}")
+
+
+def fold_on_x(a_fold, points):
     kept_points = {point for point in points if point.x < a_fold.coordinate}
     folded_points = {
         Point(x=(2 * a_fold.coordinate - point.x), y=point.y)
         for point in points
         if point.x > a_fold.coordinate
+    }
+    return kept_points.union(folded_points)
+
+
+def fold_on_y(a_fold, points):
+    kept_points = {point for point in points if point.y < a_fold.coordinate}
+    folded_points = {
+        Point(x=point.x, y=(2 * a_fold.coordinate - point.y))
+        for point in points
+        if point.y > a_fold.coordinate
     }
     return kept_points.union(folded_points)
 
