b4f9e0fde2b1a8f7ae3d0e7d3f90f899cfe71f83
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-13 09:21:03 -0500

    Parsing part 2 is done

--- src/day13.py
+++ src/day13.py
@@ -8,18 +8,24 @@ class Point(NamedTuple):
     y: int
 
     @classmethod
-    def from_string(cls, input_string):
+    def from_string(cls, input_string: str) -> "Point":
         [x, y] = input_string.strip().split(",")
         return Point(x=int(x), y=int(y))
 
 
-@pytest.mark.parametrize("input_string, expected", [
-    ("", set()),
-    ("1, 2", {Point(1, 2)}),
-    ("86,99", {Point(86, 99)}),
-    ("1,2\n3,4\n5,6\n", {Point(1, 2), Point(3, 4), Point(5, 6)}),
-    ("1,2\n3,4\n5,6\n\nfold along x=42\nfold along y=47", {Point(1, 2), Point(3, 4), Point(5, 6)}),
-])
+@pytest.mark.parametrize(
+    "input_string, expected",
+    [
+        ("", set()),
+        ("1, 2", {Point(1, 2)}),
+        ("86,99", {Point(86, 99)}),
+        ("1,2\n3,4\n5,6\n", {Point(1, 2), Point(3, 4), Point(5, 6)}),
+        (
+            "1,2\n3,4\n5,6\n\nfold along x=42\nfold along y=47",
+            {Point(1, 2), Point(3, 4), Point(5, 6)},
+        ),
+    ],
+)
 def test_parse_input_points(input_string, expected):
     assert parse_input_points(input_string) == expected
 
@@ -38,17 +44,32 @@ class Fold(NamedTuple):
     dimension: str
     coordinate: int
 
+    @classmethod
+    def from_string(cls, input_string: str) -> "Fold":
+        [dimension, coordinate] = input_string[11:].strip().split("=")
+        return Fold(dimension=dimension, coordinate=int(coordinate))
 
-@pytest.mark.parametrize("input_string, expected", [
-    ("", []),
-    ])
+
+@pytest.mark.parametrize(
+    "input_string, expected",
+    [
+        ("", []),
+        ("1,2\n\nfold along x=42", [Fold("x", 42)]),
+        ("1,2\n\nfold along x=42\nfold along y=47", [Fold("x", 42), Fold("y", 47)]),
+    ],
+)
 def test_parse_input_folds(input_string, expected):
     assert parse_input_folds(input_string) == expected
 
 
 def parse_input_folds(input_string: str) -> List[Fold]:
-    return []
+    if not input_string:
+        return []
 
+    result = []
+    for line in input_string.strip().split("\n\n")[1].split("\n"):
+        result.append(Fold.from_string(line))
+    return result
 
 
 def part_a(filepath: str):
