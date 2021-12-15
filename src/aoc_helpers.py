7663d71797a68a363dfacd9f1abb0623386870de
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-15 09:18:53 -0500

    Add some type hinting to matrix parser

--- src/aoc_helpers.py
+++ src/aoc_helpers.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 
 def test_parse_digit_matrix():
@@ -6,13 +6,13 @@ def test_parse_digit_matrix():
     assert parse_digit_matrix("123\n456") == [[1, 4], [2, 5], [3, 6]]
 
 
-def parse_digit_matrix(input_string):
+def parse_digit_matrix(input_string: str) -> List[List[int]]:
     if not input_string:
         return []
     result_transposed = []
     for line in input_string.strip().split("\n"):
         result_transposed.append([int(char) for char in line.strip()])
-    result = [[None] * len(result_transposed) for _ in result_transposed[0]]
+    result = [[0] * len(result_transposed) for _ in result_transposed[0]]
     for x in range(len(result)):
         for y in range(len(result[0])):
             result[x][y] = result_transposed[y][x]
