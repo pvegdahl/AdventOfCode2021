56d580e86e77439c1fd0d1af3bbc664e14082a8d
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-19 22:22:22 -0500

    Day 18B complete

--- src/day18.py
+++ src/day18.py
@@ -5,11 +5,14 @@ from typing import Union, Tuple, NamedTuple, Optional, List
 import pytest
 
 
-@pytest.mark.parametrize("input_string, expected", [
-    ("1", [1]),
-    ("[1,2]", [(1, 2)]),
-    ("[[[7,1],2],3]\n[[1,7],7]", [(((7,1),2),3), ((1,7),7)])
-])
+@pytest.mark.parametrize(
+    "input_string, expected",
+    [
+        ("1", [1]),
+        ("[1,2]", [(1, 2)]),
+        ("[[[7,1],2],3]\n[[1,7],7]", [(((7, 1), 2), 3), ((1, 7), 7)]),
+    ],
+)
 def test_parse_input(input_string, expected):
     assert parse_input(input_string) == expected
 
@@ -393,24 +396,40 @@ def do_homework(sfns):
     return sfn_magnitude(add_and_reduce_many_sfns(sfns))
 
 
+EXAMPLE_HOMEWORK = [
+    (((0, (5, 8)), ((1, 7), (9, 6))), ((4, (1, 2)), ((1, 4), 2))),
+    (((5, (2, 8)), 4), (5, ((9, 9), 0))),
+    (6, (((6, 2), (5, 6)), ((7, 6), (4, 7)))),
+    (((6, (0, 7)), (0, 9)), (4, (9, (9, 0)))),
+    (((7, (6, 4)), (3, (1, 3))), (((5, 5), 1), 9)),
+    ((6, ((7, 3), (3, 2))), (((3, 8), (5, 7)), 4)),
+    ((((5, 4), (7, 7)), 8), ((8, 3), 8)),
+    ((9, 3), ((9, 9), (6, (4, 9)))),
+    ((2, ((7, 7), 7)), ((5, 8), ((9, 3), (0, 2)))),
+    ((((5, 2), 5), (8, (3, 7))), ((5, (7, 5)), (4, 4))),
+]
+
+
 def test_do_homework():
-    assert (
-        do_homework(
-            [
-                (((0, (5, 8)), ((1, 7), (9, 6))), ((4, (1, 2)), ((1, 4), 2))),
-                (((5, (2, 8)), 4), (5, ((9, 9), 0))),
-                (6, (((6, 2), (5, 6)), ((7, 6), (4, 7)))),
-                (((6, (0, 7)), (0, 9)), (4, (9, (9, 0)))),
-                (((7, (6, 4)), (3, (1, 3))), (((5, 5), 1), 9)),
-                ((6, ((7, 3), (3, 2))), (((3, 8), (5, 7)), 4)),
-                ((((5, 4), (7, 7)), 8), ((8, 3), 8)),
-                ((9, 3), ((9, 9), (6, (4, 9)))),
-                ((2, ((7, 7), 7)), ((5, 8), ((9, 3), (0, 2)))),
-                ((((5, 2), 5), (8, (3, 7))), ((5, (7, 5)), (4, 4))),
-            ]
-        )
-        == 4140
-    )
+    assert do_homework(EXAMPLE_HOMEWORK) == 4140
+
+
+def do_homework_part_2(sfns):
+    max_value = 0
+    for pair in get_all_pairs(sfns):
+        max_value = max(max_value, do_homework(pair))
+    return max_value
+
+
+def get_all_pairs(sfns):
+    for i in range(len(sfns)):
+        for j in range(len(sfns)):
+            if i != j:
+                yield [sfns[i], sfns[j]]
+
+
+def test_do_homework_part_2():
+    assert do_homework_part_2(EXAMPLE_HOMEWORK) == 3993
 
 
 def part_a(filepath: str):
@@ -421,7 +440,8 @@ def part_a(filepath: str):
 
 def part_b(filepath: str):
     with open(filepath, "r") as file:
-        pass
+        sfns = parse_input(file.read())
+    return do_homework_part_2(sfns)
 
 
 if __name__ == "__main__":
