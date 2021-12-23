32476cc63106728cb32175d1bbded1d361a9dc0b
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-23 12:07:13 -0500

    [REFACTOR] Make a per player state tuple

--- src/day21.py
+++ src/day21.py
@@ -1,13 +1,22 @@
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 
 import pytest
 
 
+class PlayerState(NamedTuple):
+    position: int = 1
+    score: int = 0
+
+
 class GameState(NamedTuple):
     roll_count: int = 0
-    position_one: int = 1
+    player_states: Tuple[PlayerState] = PlayerState(),
+    the_position_one: int = 1
     score_one: int = 0
     player_turn: int = 1
+
+    def position_one(self):
+        return self.the_position_one
 
 
 @pytest.mark.parametrize(
@@ -25,23 +34,23 @@ def test_roll_count_updated(initial_game_state, expected):
 @pytest.mark.parametrize(
     "initial_game_state, expected",
     [
-        (GameState(position_one=1, roll_count=0), 7),
-        (GameState(position_one=10, roll_count=0), 6),
-        (GameState(position_one=10, roll_count=10), 6),
-        (GameState(position_one=10, roll_count=1000), 6),
-        (GameState(position_one=3, roll_count=123), 8),
+        (GameState(the_position_one=1, roll_count=0), 7),
+        (GameState(the_position_one=10, roll_count=0), 6),
+        (GameState(the_position_one=10, roll_count=10), 6),
+        (GameState(the_position_one=10, roll_count=1000), 6),
+        (GameState(the_position_one=3, roll_count=123), 8),
     ],
 )
 def test_position_one_updated(initial_game_state, expected):
-    assert one_turn(initial_game_state).position_one == expected
+    assert one_turn(initial_game_state).position_one() == expected
 
 
 @pytest.mark.parametrize(
     "initial_game_state, expected",
     [
-        (GameState(position_one=1, roll_count=0, score_one=0), 7),
-        (GameState(position_one=1, roll_count=0, score_one=10), 17),
-        (GameState(position_one=3, roll_count=123, score_one=123), 131),
+        (GameState(the_position_one=1, roll_count=0, score_one=0), 7),
+        (GameState(the_position_one=1, roll_count=0, score_one=10), 17),
+        (GameState(the_position_one=3, roll_count=123, score_one=123), 131),
     ],
 )
 def test_score_one_updated(initial_game_state, expected):
@@ -52,7 +61,7 @@ def test_player_one_not_updated_on_player_two_turn():
     initial_game_state = GameState(player_turn=2)
     next_game_state = one_turn(initial_game_state)
     assert next_game_state.score_one == initial_game_state.score_one
-    assert next_game_state.position_one == initial_game_state.position_one
+    assert next_game_state.position_one() == initial_game_state.position_one()
 
 
 @pytest.mark.parametrize(
@@ -72,7 +81,7 @@ def one_turn(initial_game_state: GameState) -> GameState:
         new_score_one = initial_game_state.score_one + new_position_one
         return GameState(
             roll_count=initial_game_state.roll_count + 3,
-            position_one=new_position_one,
+            the_position_one=new_position_one,
             score_one=new_score_one,
             player_turn=2,
         )
@@ -82,7 +91,7 @@ def one_turn(initial_game_state: GameState) -> GameState:
 
 def calculate_new_position_one(initial_game_state):
     roll_total = 3 * initial_game_state.roll_count + 6
-    new_position_one = special_mod10(initial_game_state.position_one + roll_total)
+    new_position_one = special_mod10(initial_game_state.position_one() + roll_total)
     return new_position_one
 
 
