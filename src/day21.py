389af3e8d78aff79919522b944486f10dd4335c2
Philip Vegdahl <pvegdahl@gmail.com>
2021-12-23 11:52:38 -0500

    A simple test of moving one player

--- src/day21.py
+++ src/day21.py
@@ -5,6 +5,7 @@ import pytest
 
 class GameState(NamedTuple):
     roll_count: int = 0
+    position_one: int = 1
 
 
 @pytest.mark.parametrize("initial_game_state, expected_roll_count", [
@@ -16,8 +17,30 @@ def test_roll_count_updated(initial_game_state, expected_roll_count):
     assert one_turn(initial_game_state).roll_count == expected_roll_count
 
 
+@pytest.mark.parametrize("initial_game_state, expected_position_one", [
+    (GameState(position_one=1, roll_count=0), 7),
+    (GameState(position_one=10, roll_count=0), 6),
+    (GameState(position_one=10, roll_count=10), 6),
+    (GameState(position_one=10, roll_count=1000), 6),
+    (GameState(position_one=3, roll_count=123), 8),
+])
+def test_position_one_updated(initial_game_state, expected_position_one):
+    assert one_turn(initial_game_state).position_one == expected_position_one
+
+
 def one_turn(initial_game_state: GameState) -> GameState:
-    return GameState(roll_count=initial_game_state.roll_count+3)
+    new_position_one = calculate_new_position_one(initial_game_state)
+    return GameState(roll_count=initial_game_state.roll_count+3, position_one=new_position_one)
+
+
+def calculate_new_position_one(initial_game_state):
+    roll_total = 3 * initial_game_state.roll_count + 6
+    new_position_one = special_mod10(initial_game_state.position_one + roll_total)
+    return new_position_one
+
+
+def special_mod10(number: int) -> int:
+    return (number-1) % 10 + 1
 
 
 def part_a(filepath: str):
